161e8823deeeb34e7917c47e0b9b5aa40e41f636
Anaslbkh <anasdhj608@gmail.com>
2025-05-30 19:44:27 +0100

    update the poup

---
+++ popup.js
@@ -0,0 +1,7 @@
+// popup.js
+document.addEventListener("DOMContentLoaded", () => {
+  const currentYearSpan = document.getElementById("current-year");
+  if (currentYearSpan) {
+    currentYearSpan.textContent = new Date().getFullYear().toString();
+  }
+});
